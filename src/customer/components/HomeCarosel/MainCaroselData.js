0519f75e6cd9634f7aa60762d9686c4e8df183a0
HackerRP07 <riteshpawar2333@gmail.com>
2023-08-09 23:14:13 +0530

    initial commit

---
+++ src/customer/components/HomeCarosel/MainCaroselData.js
@@ -0,0 +1,14 @@
+export const MainCaroselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_6.jpg",
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_9.jpg",
+    },
+    {
+        image : "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/8/_/8_8.jpg",
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_8.jpg",
+    }
+]
---
+++ src/customer/pages/Homepage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainCarosel from '../../components/HomeCarosel/MainCarosel';
+
+const HomePage = () => {
+    return (
+    <div>
+        <MainCarosel/>
+
+        <div>
+            
+        </div>
+    </div>
+    )
+}
+
+export default HomePage;
